cd6a2a510e2ff96bef161b059f55af1bfdc05556
Rahul Godara <godara.nanu@gmail.com>
2024-12-22 13:57:07 +0530

    added requirements.txt file

--- src/__init__.py
+++ src/__init__.py
@@ -2,40 +2,9 @@
 Clinical Explainer Package for Multiple Choice Medical Questions
 """
 
-import importlib
-import subprocess
-import sys
-
-def import_or_install(package_name, version=None):
-    try:
-        if version:
-            package_spec = f"{package_name}>={version}"
-        else:
-            package_spec = package_name
-
-        # Attempt to import the package
-        return importlib.import_module(package_name)
-    except ImportError:
-        # If import fails, install the package
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package_spec])
-        # Try importing again after installation
-        return importlib.import_module(package_name)
-    
-# Required packages with versions where needed
-required_packages = [
-    ('datasets', None),
-    ('transformers', None),
-    ('torch', None),
-    ('lime', None),
-    ('shap', None),
-    ('tokenshap', None),
-    ('bitsandbytes', '0.41.1')  # Specify minimum version for 8-bit quantization
-]
-
-for package, version in required_packages:
-    import_or_install(package, version)
-
-# Now import the Pipeline
 from .pipeline import Pipeline
+from .strategies import ModelType
 
 __version__ = '1.1.1'
+
+__all__ = ['Pipeline', 'ModelType']
